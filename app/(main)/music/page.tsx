a0bf7253c8a9779a1b8bf3833c6791b2df480e28
Stukov-Alexis <stukovedward@gmail.com>
2025-08-04 20:26:17 +0700

    Revert test

--- app/(main)/music/page.tsx
+++ app/(main)/music/page.tsx
@@ -85,8 +85,8 @@ const MusicPage = async () => {
                     tracks={album.tracks}
                     src={
                       album.image
-                        ? `https://maraxktbvnihgopdjmnx.supabase.co/storage/v1/object/public/icon/${album.image}`
-                        : "/icon.jpg"
+                        ? `https://maraxktbvnihgopdjmnx.supabase.co/storage/v1/object/public/music-files/${album.image}`
+                        : "public/BG_CS_Arona_04.jpg"
                     }
                     alt={album.title}
                     width={720}
