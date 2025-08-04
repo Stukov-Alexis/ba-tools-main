336597ce6b587b995983a97e99e4b8393e34b022
Stukov-Alexis <stukovedward@gmail.com>
2025-08-04 15:13:22 +0700

    Fix Crash 404

--- app/lib/music/sortTracks.ts
+++ app/lib/music/sortTracks.ts
@@ -1,14 +1,15 @@
 import type { ITrack } from "@/app/type/music-type";
 
 function extractNumberFromId(title: string): number {
-  const getNumber = title.split(" ")[1];
-  return getNumber ? Number.parseInt(getNumber, 10) : 0;
+  const parts = title.split(" ");
+  return parts[1] ? Number.parseInt(parts[1], 10) : 0;
 }
 
 export function sortTracksById(tracks: ITrack[]): ITrack[] {
   return tracks.slice().sort((a, b) => {
-    const numA = extractNumberFromId(a.id);
-    const numB = extractNumberFromId(b.id);
+    const numA = extractNumberFromId(a.title);
+    const numB = extractNumberFromId(b.title);
     return numA - numB;
   });
 }
+
