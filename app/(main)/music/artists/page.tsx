10405e19f2355e88fe10a5824d6a9b726d547d0f
Stukov-Alexis <stukovedward@gmail.com>
2025-08-05 20:29:29 +0700

    Updating artist page to shown artist

--- app/(main)/music/artists/page.tsx
+++ app/(main)/music/artists/page.tsx
@@ -4,37 +4,43 @@ import { getAlbumsBySection } from "@/app/lib/fetcher/getAlbum";
 import type { IAlbum } from "@/app/type/music-type";
 
 const ArtistsPage = async () => {
+  // Fetch albums where section = 'artist'
   const albums = await getAlbumsBySection("artist");
 
   return (
     <div>
       <h2 className="text-4xl font-bold mb-4">Artist</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 lg:gap-4">
-        {albums.map((album: IAlbum) => (
-          <Link
-            href={`/music/${album.path}`}
-            key={album.id}
-            className="flex flex-col gap-2 hover:bg-primary/50 rounded-xl p-2 group transition-colors duration-200"
-            draggable={false}
-          >
-            <AlbumImage
-              tracks={album.tracks}
-              src={
-                album.image
-                  ? `https://jyxwxdxjdshypymisxeo.supabase.co/storage/v1/object/public/music/images/${album.image}`
-                  : "/icon.png"
-              }
-              alt={album.title}
-              width={256}
-              height={256}
-              isVisible={false}
-            />
-            <div className="px-2 pb-2">
-              <p className="font-bold capitalize line-clamp-2">{album.title}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
+
+      {albums.length === 0 ? (
+        <p className="text-gray-500">No artist albums found.</p>
+      ) : (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 lg:gap-4">
+          {albums.map((album: IAlbum) => (
+            <Link
+              href={`/music/${album.path}`}
+              key={album.id}
+              className="flex flex-col gap-2 hover:bg-primary/50 rounded-xl p-2 group transition-colors duration-200"
+              draggable={false}
+            >
+              <AlbumImage
+                tracks={album.tracks}
+                src={
+                  album.image
+                    ? `https://maraxktbvnihgopdjmnx.supabase.co/storage/v1/object/public/icon/${album.image}`
+                    : "/icon.png"
+                }
+                alt={album.title}
+                width={256}
+                height={256}
+                isVisible={false}
+              />
+              <div className="px-2 pb-2">
+                <p className="font-bold capitalize line-clamp-2">{album.title}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
