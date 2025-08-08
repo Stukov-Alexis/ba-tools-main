1fa6d1ce54d3ca4f709356b095229dcb92e63471
Stukov-Alexis <stukovedward@gmail.com>
2025-08-08 19:58:20 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "jyxwxdxjdshypymisxeo.supabase.co",
+        hostname: "maraxktbvnihgopdjmnx.supabase.co",
         pathname: "/storage/**",
       },
     ],
